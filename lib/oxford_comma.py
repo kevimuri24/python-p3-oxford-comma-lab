6bca7769b5aae138dfb15fff9cf5f3ab0ed57b0e
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-09-29 22:31:08 -0400

    Finished Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    if len(items) > 2:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+
